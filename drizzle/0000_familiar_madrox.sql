d9f6e1f6e60772e595a7fffdb477c3e5a5a48165
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2025-12-27 14:14:12 -0400

    feat: Add migrations

---
+++ drizzle/0000_familiar_madrox.sql
@@ -0,0 +1,22 @@
+CREATE TABLE "books" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(256) NOT NULL,
+	"image_url" varchar(512),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "chapters" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"book_id" uuid NOT NULL,
+	"number" integer NOT NULL,
+	"title" varchar(256) NOT NULL,
+	"content" varchar(1000000) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "chapters_book_id_number_unique" UNIQUE("book_id","number")
+);
+--> statement-breakpoint
+ALTER TABLE "chapters" ADD CONSTRAINT "chapters_book_id_books_id_fk" FOREIGN KEY ("book_id") REFERENCES "public"."books"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "books_title_idx" ON "books" USING btree ("title");--> statement-breakpoint
+CREATE INDEX "chapters_book_id_idx" ON "chapters" USING btree ("book_id");
